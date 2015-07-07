858bf78ea9072fa530f2bb32f0bfb41fde911c97
tazkaz <tazkaz@tazkaz-HP>
2015-07-07 13:42:25 +0900

    test

--- java/src/main/java/java/test.java
+++ java/src/main/java/java/test.java
@@ -5,6 +5,8 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
 	}
 
 }
